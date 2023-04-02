44b94203222474b10ef0820a12a20a39fc32ccb0
aivaraskriksciunas <aiviskri@gmail.com>
2023-04-02 22:04:00 +0300

    Removed my old code that I added by accident

--- flashcards/app/Models/User.php
+++ flashcards/app/Models/User.php
@@ -35,20 +35,4 @@ class User extends Model implements AuthenticatableContract
     public function decks() {
         return $this->hasMany( Deck::class );
     }
-
-    public function getLibrary() 
-    {
-        return Library::where( 'user_id', $this->id )
-            // ->where( 'parent_id', null )
-            ->firstOr( function() { return $this->createLibrary(); } );
-    }
-
-    public function createLibrary() 
-    {
-        $library = new Library([ 'name' => $this->name . " Library" ]);
-        $library->user()->associate( $this );
-        $library->save();
-
-        return $library;
-    }
 }
