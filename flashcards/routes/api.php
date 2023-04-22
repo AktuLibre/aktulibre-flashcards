b9dc663a1ee3a55d86064e41889fb44ad915b90f
FlickerX <nikitagorcakov14@gmail.com>
2023-04-22 20:53:18 +0300

    Nikita Created Api endpoint for libraries

--- flashcards/routes/api.php
+++ flashcards/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ApiAuthController;
+use App\Http\Controllers\Api\ApiLibraryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,4 +21,7 @@ Route::post( '/register', [ ApiAuthController::class, 'register' ]);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+Route::middleware( 'auth:sanctum' )->group( function() {
+    Route::get( '/library', [ ApiLibraryController::class, 'index' ]);
 });
