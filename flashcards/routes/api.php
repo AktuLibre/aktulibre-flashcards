68cd2d6dba7b887f425becb3fe6ff86c7be822e7
FlickerX <nikitagorcakov14@gmail.com>
2023-04-23 21:52:27 +0300

    Nikita 861mkrxmn fix

--- flashcards/routes/api.php
+++ flashcards/routes/api.php
@@ -22,6 +22,3 @@ Route::post( '/register', [ ApiAuthController::class, 'register' ]);
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::middleware( 'auth:sanctum' )->group( function() {
-    Route::get( '/library', [ ApiLibraryController::class, 'index' ]);
-});
