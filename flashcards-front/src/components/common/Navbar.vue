925344b8806479b968841f4d8f447e271c00c2ba
FlickerX <nikitagorcakov14@gmail.com>
2023-04-23 21:43:55 +0300

    Nikita 861mkrxmn

--- flashcards-front/src/components/common/Navbar.vue
+++ flashcards-front/src/components/common/Navbar.vue
@@ -8,7 +8,7 @@ const { user } = useUserStore()
 <template>
     
 <nav class="main-navbar border-b">
-    <div class="container flex py-4">
+    <!-- <div class="container flex py-4">
         <div class="navbar-logo flex-grow">
             Flashcards
         </div>
@@ -16,7 +16,38 @@ const { user } = useUserStore()
         <div class="">
             {{ user.name }}
         </div>
+    </div> -->
+    <div class="container-nav flex py-4">
+        <!-- <div class="navbar-logo flex-grow"> -->
+            <button @click="createNewDeck" class="button">New Deck</button>
+            <div class="">
+                {{ user.name }}
+            </div>
+        <!-- </div> -->
     </div>
 </nav>
 
 </template>
+<style>
+.button{
+    width: 100%;
+    background-color: var( --color-primary ) !important;
+    border-radius: 1000px;
+    display: inline-block;
+    padding: 8px 18px;
+    color: white;
+    user-select: none;
+    font-weight: 500;
+    margin-right: 25px;
+}
+.main-navbar{
+    display: flex;
+    /* position: relative; */
+}
+.container-nav{
+    display: flex;
+    align-items: center;
+    margin-left: auto;
+    margin-right: 100px;
+}
+</style>
---
+++ flashcards-front/src/components/common/Sidebar.vue
@@ -0,0 +1,48 @@
+<script setup>
+import { computed } from 'vue';
+// import { useUserSettingStore } from "../../stores/user-settings";
+
+const logo = "/LOGO.png"
+</script>
+<template>
+    <div class="sidebar">
+        <router-link :to="{name: 'home'}">
+            <div class="sidebar-logo">
+                <img :src="logo">
+            </div>
+        </router-link>
+        <div class="px-8">
+            <div class="links">
+                <router-link :to="{name: 'home'}">
+                    <div class='sidebar-link'>Home</div>
+                </router-link>
+                <router-link :to="{name: 'home'}">
+                    <div class='sidebar-link'>Profile</div>
+                </router-link>
+                <router-link :to="{name: 'home'}">
+                    <div class='sidebar-link'>Forum</div>
+                </router-link>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.sidebar {
+    background-color: var( --color-white );
+    height: 100%;
+}
+.sidebar-logo {
+    @apply p-8;
+    min-height: 80px;
+    text-align: center;
+}
+.links {
+    margin-bottom: 40px;
+}
+.sidebar-link {
+    font-weight: 500;
+    font-size: 18px;
+    margin: 15px 0;
+}
+</style>
